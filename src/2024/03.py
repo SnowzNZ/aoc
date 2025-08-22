f77ff8ddea9a9093707061f9a7d468295f46d2fa
SnowzNZ <98007575+SnowzNZ@users.noreply.github.com>
2025-08-22 14:02:45 +1200

    day 3 part 2

--- src/2024/03.py
+++ src/2024/03.py
@@ -2,14 +2,57 @@ import re
 
 from aocd import data
 
-pattern = r"mul\((\d+),(\d+)\)"
-matches = re.finditer(pattern, data)
 
-total = 0
+def part_1() -> int:
+    mul_pattern = r"mul\((\d+),(\d+)\)"
+    mul_matches = re.finditer(mul_pattern, data)
 
-for match in matches:
-    num1 = int(match.group(1))
-    num2 = int(match.group(2))
-    total += num1 * num2
+    total = 0
 
-print(total)
+    for match in mul_matches:
+        num1 = int(match.group(1))
+        num2 = int(match.group(2))
+        total += num1 * num2
+
+    return total
+
+
+def part_2() -> int:
+    do_pattern = r"do\(\)"
+    dont_pattern = r"don't\(\)"
+
+    instructions = []
+
+    for match in re.finditer(do_pattern, data):
+        instructions.append((match.start(), "do"))
+
+    for match in re.finditer(dont_pattern, data):
+        instructions.append((match.start(), "dont"))
+
+    instructions.sort(key=lambda x: x[0])
+
+    mul_pattern = r"mul\((\d+),(\d+)\)"
+    mul_matches = list(re.finditer(mul_pattern, data))
+
+    total = 0
+
+    for mul_match in mul_matches:
+        mul_pos = mul_match.start()
+
+        enabled = True
+
+        for control_pos, control_type in reversed(instructions):
+            if control_pos < mul_pos:
+                enabled = control_type == "do"
+                break
+
+        if enabled:
+            num1 = int(mul_match.group(1))
+            num2 = int(mul_match.group(2))
+            total += num1 * num2
+
+    return total
+
+
+print(part_1())
+print(part_2())
